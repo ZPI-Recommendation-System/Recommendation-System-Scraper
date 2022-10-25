6ec398b14b76ca9a3c50a5aad61603f85d5f62f7
skateko <carino9@tlen.pl>
2022-10-25 20:47:51 +0200

    #30m7aug

--- mergeAllegroUB.py
+++ mergeAllegroUB.py
@@ -35,10 +35,14 @@ laptops_data['Tokens'] = [[item for sublist in [re.sub('TI$', ' TI', token.upper
 # In[7]:
 
 
-gpu_benchmark_data['Tokens'] = gpu_benchmark_data[['Brand','Model']].apply(lambda x: " ".join(x) if(x[0] not in x[1]) else x[1], axis =1)
+gpu_benchmark_data['Tokens'] = gpu_benchmark_data[['Brand','Model']].apply(lambda x: " ".join(x) if(x[0] not in x[1]) else x[1], axis=1)
 gpu_benchmark_data['Tokens'] = gpu_benchmark_data['Tokens'].str.upper()
 gpu_benchmark_data['Tokens'] = gpu_benchmark_data['Tokens'].str.replace(r"[\[\]'!@#$\";()]", '', regex=True)
-gpu_benchmark_data['Tokens'] = [[item for sublist in [re.sub('-Ti$', ' TI', token.upper(), flags=re.IGNORECASE).split() if re.findall('-TI$', token, re.IGNORECASE) else [token.upper()] for token in model_token] for item in sublist] for model_token in gpu_benchmark_data['Tokens'].str.split()]
+gpu_benchmark_data['Tokens'] = [[item for sublist in [re.sub(r'[-/]', ' ', token.upper(), flags=re.IGNORECASE).split() if re.findall(r'[-/]', token, re.IGNORECASE) else [token.upper()] for token in model_token] for item in sublist] for model_token in gpu_benchmark_data['Tokens'].str.split()]
+#gpu_benchmark_data['Tokens'] = [[item for sublist in [re.sub('-Ti$', ' TI', token.upper(), flags=re.IGNORECASE).split() if re.findall('-TI$', token, re.IGNORECASE) else [token.upper()] for token in model_token] for item in sublist] for model_token in gpu_benchmark_data['Tokens'].str.split()]
+#gpu_benchmark_data['Tokens'] = gpu_benchmark_data['Tokens'].apply(lambda x: [re.sub('[-/]', ' ', token.upper(), flags=re.IGNORECASE) for token in x])
+# gpu_benchmark_data[pd.DataFrame(gpu_benchmark_data['Tokens'].tolist()).isin(['-']).any(1).values]
+
 [x for x in gpu_benchmark_data['Tokens']]
 
 
