4f8467ec1f1cc80911b7cdf6cfffba5e64751060
skateko <carino9@tlen.pl>
2022-10-25 21:22:12 +0200

    #30m7aug

--- mergeAllegroUB.py
+++ mergeAllegroUB.py
@@ -8,22 +8,20 @@ from pprint import pprint
 import regex as re
 import pandas as pd
 
-laptops_data = pd.read_csv('clear-laptops.csv')
-cpu_bechmark_data = pd.read_csv('CPU_UserBenchmarks.csv')
-gpu_benchmark_data = pd.read_csv('GPU_UserBenchmarks.csv')
+import constants
+
+laptops_data = pd.read_csv(constants.LAPTOPS_FILE_PATH)
+cpu_bechmark_data = pd.read_csv(constants.CPU_BENCHMARK_FILE_PATH)
+gpu_benchmark_data = pd.read_csv(constants.GPU_BENCHMARK_FILE_PATH)
 laptops_data.columns = laptops_data.columns.str.replace(r'\s+', '_')
 
-all_tokens_gpu = dict()
+all_tokens_gpu = set()
 
 
 # In[2]:
 
 
-stop_words = ['NVIDIA','INTEL','AMD', 'APPLE']
-
-
-# In[3]:
-
+#Tworzenie tokenów z pliku z danymi laptopów
 
 laptops_data['Tokens'] = laptops_data['Model_karty_graficznej'].str.replace(r"[\[\]'!@#$\";()]", '', regex=True)
 laptops_data['Tokens'] = laptops_data['Tokens'].str.upper()
@@ -32,8 +30,10 @@ laptops_data['Tokens'] = [[item for sublist in [re.sub('TI$', ' TI', token.upper
 [x for x in laptops_data['Tokens']]
 
 
-# In[7]:
+# In[3]:
 
+
+#Tworzenie tokenów z pliku benchmarkowego
 
 gpu_benchmark_data['Tokens'] = gpu_benchmark_data[['Brand','Model']].apply(lambda x: " ".join(x) if(x[0] not in x[1]) else x[1], axis=1)
 gpu_benchmark_data['Tokens'] = gpu_benchmark_data['Tokens'].str.upper()
@@ -41,19 +41,33 @@ gpu_benchmark_data['Tokens'] = gpu_benchmark_data['Tokens'].str.replace(r"[\[\]'
 gpu_benchmark_data['Tokens'] = [[item for sublist in [re.sub(r'[-/]', ' ', token.upper(), flags=re.IGNORECASE).split() if re.findall(r'[-/]', token, re.IGNORECASE) else [token.upper()] for token in model_token] for item in sublist] for model_token in gpu_benchmark_data['Tokens'].str.split()]
 #gpu_benchmark_data['Tokens'] = [[item for sublist in [re.sub('-Ti$', ' TI', token.upper(), flags=re.IGNORECASE).split() if re.findall('-TI$', token, re.IGNORECASE) else [token.upper()] for token in model_token] for item in sublist] for model_token in gpu_benchmark_data['Tokens'].str.split()]
 #gpu_benchmark_data['Tokens'] = gpu_benchmark_data['Tokens'].apply(lambda x: [re.sub('[-/]', ' ', token.upper(), flags=re.IGNORECASE) for token in x])
-# gpu_benchmark_data[pd.DataFrame(gpu_benchmark_data['Tokens'].tolist()).isin(['-']).any(1).values]
+
 
 [x for x in gpu_benchmark_data['Tokens']]
+# gpu_benchmark_data[pd.DataFrame(gpu_benchmark_data['Tokens'].tolist()).isin(['-']).any(1).values]
+
+
+# In[4]:
+
+
+#Wypisanie tokenów
+
+[all_tokens_gpu.add(x) for sublist in gpu_benchmark_data['Tokens'] for x in sublist]
+[all_tokens_gpu.add(x) for sublist in laptops_data['Tokens'] for x in sublist]
+all_tokens_gpu
 
 
 # In[5]:
 
 
+#Sprawdzenie czy gdzies zostaly jakiekolwiek niepożądane znaki
+
+checks = [x if re.findall(r"[\[\]'!@#$\";()\s]", x) else None for sublist in gpu_benchmark_data['Tokens'] for x in sublist]
+[x for x in checks if x is not None]
+
+
+# In[ ]:
 
 
 
-# In[6]:
-
-
-print(re.search('-TI$', '3090-Ti', re.IGNORECASE))
 
