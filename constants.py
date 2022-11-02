a7d755aecc7824ac0642936fe2d7ca29eb71d3b9
skateko <carino9@tlen.pl>
2022-11-02 20:24:57 +0100

    #31gyn5z

---
+++ constants.py
@@ -0,0 +1,8 @@
+LAPTOPS_FILE_PATH = r'C:\Users\Jakub\Desktop\ZPI\Scapper-System\clear-laptops.csv'
+CPU_BENCHMARK_FILE_PATH = r'C:\Users\Jakub\Desktop\ZPI\Scapper-System\CPU_UserBenchmarks.csv'
+GPU_BENCHMARK_FILE_PATH = r'C:\Users\Jakub\Desktop\ZPI\Scapper-System\GPU_UserBenchmarks.csv'
+
+
+
+
+
