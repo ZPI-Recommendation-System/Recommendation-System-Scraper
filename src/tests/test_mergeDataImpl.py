4bf9ef2aff9883b5c68a1229ed91f42d7c5976e5
skateko <carino9@tlen.pl>
2022-11-13 22:19:56 +0100

    Tests for tokenization - first steps

--- src/tests/test_mergeDataImpl.py
+++ src/tests/test_mergeDataImpl.py
@@ -1,2 +1,24 @@
-def test_create_positions_dict():
-    assert False
+import pytest
+
+
+class MergeDataComponentsTests:
+    def test_create_set_of_tokens(self):
+        assert False
+
+    def test_create_positions_dict(self):
+        assert False
+
+    def test_create_vector(self):
+        assert False
+
+    def test_create_vectors_df(self):
+        assert False
+
+    def test_create_vectors(self):
+        assert False
+
+    def test_cosine_score(self):
+        assert False
+
+    def test_create_assignment_dict(self):
+        assert False
---
+++ src/tests/test_mergeGPUData.py
@@ -0,0 +1,15 @@
+class MergeAllegroGPUTests:
+    def test_create_laptop_token_column(self):
+        assert False
+
+    def test_create_laptops_tokens(self):
+        assert False
+
+    def test_create_benchmark_tokens(self):
+        assert False
+
+    def test_test_tokens(self):
+        assert False
+
+    def test_print_assigns(self):
+        assert False
